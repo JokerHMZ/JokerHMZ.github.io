39a492a2a0ff9b09015723fcdeffa3bf1a4d1d8c
JokerHMZ <610678466@qq.com>
2017-09-13 15:04:18 +0800

    第二次作业

---
+++ yideng2/yideng_phpserver/yideng2/main.php
@@ -0,0 +1,18 @@
+<?php
+	header("Content-Type:application/json");
+	$tags_id = $_REQUEST['id'];
+	$conn = mysqli_connect('localhost','root','','tags') ;
+	$sql = "SET NAMES UTF8";
+	mysqli_query($conn, $sql);
+	$sql = "SELECT count FROM tagnumber WHERE id='$tags_id'";   
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_array($result);
+	$count=$row['count']+1;
+	$sql = "UPDATE tagnumber SET count='$count' WHERE id='$tags_id'";
+	$result = mysqli_query($conn, $sql);
+	if($row){
+		echo $result;
+	}else{
+		echo '-1';
+	}
+?>
